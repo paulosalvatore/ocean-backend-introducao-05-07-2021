5334d7963e32c73da89dbae5ae19c15050831d74
Paulo Salvatore <salvatoregames@gmail.com>
2021-07-07 11:08:01 -0300

    Endpoint Read All

--- index.js
+++ index.js
@@ -5,4 +5,27 @@ app.get('/hello', function (req, res) {
   res.send('Hello World')
 })
 
+/*
+Lista de Endpoints CRUD
+Create, Read (Single & All), Update, Delete
+Criar, Ler (Individual & Tudo), Atualizar, Remover
+
+Associamos os endpoints aos verbos de HTTP
+Quando seguimos as convenções, utilizandos os verbos corretos,
+podemos dizer que a nossa aplicação segue os padrões REST
+Quando uma aplicação segue os padrões REST, ela é chamada de RESTful
+[POST] -> Create
+[GET] -> Read
+[PUT/PATCH] -> Update
+[DELETE] -> Delete
+*/
+
+// Escolha um tema: Filmes, Séries, Jogos, etc
+
+const lista = ["Senhor dos Anéis", "Harry Potter"];
+
+app.get("/filmes", function (req, res) {
+  res.send(lista)
+})
+
 app.listen(3000)
